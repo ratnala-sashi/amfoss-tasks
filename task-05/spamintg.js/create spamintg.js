879f325591b96e169541019353d362da8b4a1775
ratnala-sashi <74501734+ratnala-sashi@users.noreply.github.com>
2020-11-27 21:47:07 +0530

    Update create spamintg.js

--- task-05/spamintg.js/create spamintg.js
+++ task-05/spamintg.js/create spamintg.js
@@ -1,1 +1,14 @@
-
+var message = ""; //enter your message here 
+var interval = 1  ; // enter in seconds 
+var count = 10 ; //number of times u wanna spam
+var notifyInterval = 5 ; 
+var i = 0 ;
+var timer = setInterval(function(){
+	document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = message;
+	$('.im_submit').trigger('mousedown');	
+	i++;
+	if( i  == count )
+	clearInterval(timer);
+	if( i % notifyInterval == 0)
+	console.log(i + ' MESSAGES SENT');
+} , interval * 1000 ) ;
